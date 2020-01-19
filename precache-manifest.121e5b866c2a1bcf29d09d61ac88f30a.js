c2998000f89b16b28c064115cd5a7f0cd4d7e498
unknown <quangnguyen5598@gmail.com>
2020-01-19 17:10:27 -0500

    Updates

---
+++ precache-manifest.121e5b866c2a1bcf29d09d61ac88f30a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7ba0a9b8e99dffdc3359bdb708b273",
+    "url": "/book-information-page/index.html"
+  },
+  {
+    "revision": "e44849fe4fc3db7a81ba",
+    "url": "/book-information-page/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6f1023745f4316adc215",
+    "url": "/book-information-page/static/css/main.6e5d321c.chunk.css"
+  },
+  {
+    "revision": "e44849fe4fc3db7a81ba",
+    "url": "/book-information-page/static/js/2.00e3f253.chunk.js"
+  },
+  {
+    "revision": "881be2d35f8ec5c3e57b88bfa92ed98e",
+    "url": "/book-information-page/static/js/2.00e3f253.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f1023745f4316adc215",
+    "url": "/book-information-page/static/js/main.a9af7782.chunk.js"
+  },
+  {
+    "revision": "effb2a595ff21e928840",
+    "url": "/book-information-page/static/js/runtime-main.778762aa.js"
+  },
+  {
+    "revision": "c5cc94e972b902205aea9d955a124d45",
+    "url": "/book-information-page/static/media/magic-book.c5cc94e9.png"
+  },
+  {
+    "revision": "b930a63a23f31c4dcca568a7ebbac447",
+    "url": "/book-information-page/static/media/overlay.b930a63a.png"
+  },
+  {
+    "revision": "db4ff89e0cd9acd378b34ccf06ffbdba",
+    "url": "/book-information-page/static/media/so-white.db4ff89e.png"
+  }
+]);
